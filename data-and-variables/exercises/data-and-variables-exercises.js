b012d2d2cf4becbfd8591b4c3289d740190c6a27
AAnusha26 <adabalaanusha26@gmail.com>
2024-06-12 23:35:21 -0400

    Calcuting distance to moon

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,22 @@
-// Declare and assign the variables below
-
-// Use console.log to print the 'typeof' each variable. Print one item per line.
-
-// Calculate a space mission below
-
-// Print the results of the space mission calculations below
-
-// Calculate a trip to the moon below
-
-// Print the results of the trip to the moon below
+let shuttleName = 'Determination';
+let shuttleSpeedMph = 17500;
+let distanceToMarsKm = 225000000;
+let distanceToMoonKm = 38400; 
+const milesPerKilometer = 0.621;
+console.log(typeof shuttleName); // string
+console.log(typeof shuttleSpeedMph); // number
+console.log(typeof distanceToMarsKm); // number
+console.log(typeof distanceToMoonKm); // number
+console.log(typeof milesPerKilometer); // number
+let milesToMars = distanceToMarsKm * milesPerKilometer;
+console.log(milesToMars);
+let hoursToMars = milesToMars / shuttleSpeedMph;
+console.log(hoursToMars);
+let daysToMars = hoursToMars / 24;
+console.log(shuttleName + " will take " + daysToMars + " days to reach the Mars. ");
+let milesToMoon = distanceToMoonKm *milesPerKilometer;
+console.log(milesToMoon);
+let hoursToMoon = milesToMoon / shuttleSpeedMph;
+console.log(hoursToMoon);
+let daysToMoon = hoursToMoon / 24;
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon. ");
